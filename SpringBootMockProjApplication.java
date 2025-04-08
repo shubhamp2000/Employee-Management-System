29d99898705b2f30a325375345470b02790caf98
Shubham Patil <142170372+shubhamp2000@users.noreply.github.com>
2025-04-08 13:21:06 +0530

    Add files via upload

---
+++ SpringBootMockProjApplication.java
@@ -0,0 +1,13 @@
+package com.itv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMockProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMockProjApplication.class, args);
+	}
+
+}
